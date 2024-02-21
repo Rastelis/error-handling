1586992f8812488d7328a3c8c6345e69039b8f46
LaimisKirsa <laimisa@gmail.com>
2024-02-22 00:37:38 +0200

    finished task

--- task-one/task-one.js
+++ task-one/task-one.js
@@ -1,1 +1,12 @@
-document.querySelector("body").innerHTML=' <div class="wraper"><h4>Write a JavaScript function that takes a number as a parameter and throws a custom \'Error\' if the number is not an integer.</h4></div>'
+document.querySelector("body").innerHTML = ' <div class="wraper"><h4>Write a JavaScript function that takes a number as a parameter and throws a custom \'Error\' if the number is not an integer.</h4></div>'
+const input = 5.5
+
+checkIfInteger(input)
+
+
+function checkIfInteger(input) {
+    if (!Number.isInteger(input)) {
+        throw new Error('Invalid number. Please input an integer.');
+    }
+    console.log('Number is valid:', input);
+}
