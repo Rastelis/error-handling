a5ea78ae702f7e95daf857e21b23de56d5c4f76b
LaimisKirsa <laimisa@gmail.com>
2024-02-22 00:49:34 +0200

    created new task directory

---
+++ task-two/task-two.js
@@ -0,0 +1,3 @@
+document.querySelector("body").innerHTML = '<div class="wraper"><h4>'
+'Write a JavaScript program that uses a try-catch block to catch and handle a \'TypeError\' when accessing a property of an undefined object.'
+'</h4></div>';
