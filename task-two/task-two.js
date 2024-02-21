bf44547c8238f656542366bac50d4d50f9466f63
LaimisKirsa <laimisa@gmail.com>
2024-02-22 01:02:45 +0200

    finished task

--- task-two/task-two.js
+++ task-two/task-two.js
@@ -1,3 +1,14 @@
-document.querySelector("body").innerHTML = '<div class="wraper"><h4>'
-'Write a JavaScript program that uses a try-catch block to catch and handle a \'TypeError\' when accessing a property of an undefined object.'
-'</h4></div>';
+document.querySelector("body").innerHTML = '<div class="wraper"><h4>\
+Write a JavaScript program that uses a try-catch block to catch and handle a \'TypeError\' when accessing a property of an undefined object.\
+</h4></div>';
+try {
+    const undefinedObject = undefined;
+    console.log(undefinedObject.property);
+} catch (error) {
+    if (error instanceof TypeError) {
+        console.log('Error: Property access to undefined object');
+    }
+    else {
+        throw error
+    }
+}
